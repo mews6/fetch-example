3be4202e34b6d8215ae641dbf16344ecbc3eb571
Jaime Torres <156240086+mews6@users.noreply.github.com>
2024-08-20 10:18:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Fetch Example
+
+A little example for the utilization of Node's 'Fetch' command. Meant to interoperate with Bootstrap on a neat, little 
+
+### Build requirements
+
+This project, as simple as it is happens to still require a version of node.js equal or newer to 18. This is because the 'fetch'  API was not natively implemented before that version. You can get it either through [Node's official website](https://nodejs.org/en) or, as i tend to prefer, if you use a UNIX system, such as MacOS or Linux, through [nvm (Node Version Manager)](https://github.com/nvm-sh/nvm). In the case of the latter, you can get node itself post-installing nvm by running the following command:
+
+```
+nvm install 20
+```
---
+++ src/pedidos.js
@@ -0,0 +1,37 @@
+function calcularPopularidad(pedidos, productos) {
+    const ret = [];
+
+    for (let i = 0; i < pedidos.length; i++) {
+        let obj = pedidos[i];
+        if !(ret.includes(obj.idproducto)) {
+            let producto = productos.idproducto[obj.idproducto];
+            ret.push(
+                {
+                    "nombre": producto.nombreProducto;
+                    "idproducto": obj.idproducto,
+                    "cantidad": int(obj.cantidad)
+                }
+            )
+        } else {
+            ret.idproducto[obj.idproducto] += int(obj.cantidad);
+        }
+    } return ret;
+    
+}
+
+function pedidosNecesarios( ){
+    const pedidos = fetch('https://gist.githubusercontent.com/josejbocanegra/7b6febf87e9d986048a648487b35e693/raw/576531a2d0e601838fc3de997e021816a4b730f8/detallePedido.json')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+    const productos = fetch('https://gist.githubusercontent.com/josejbocanegra/be0461060d1c2d899740b8247089ba22/raw/916d2141e32e04031bda79c8886e8e4df0ae7f24/productos.json')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+    calcularPopularidad(pedidos, productos)
+
+}
+
+
+
+pedidosNecesarios();
