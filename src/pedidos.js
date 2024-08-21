0a581bde0525405158f8c6bfa3d7fdd038ecde45
Jaime Torres <156240086+mews6@users.noreply.github.com>
2024-08-21 00:33:38 -0500

    change some things about syntax

--- src/pedidos.js
+++ src/pedidos.js
@@ -1,9 +1,9 @@
 function calcularPopularidad(pedidos, productos) {
-    const ret = [];
+    let ret = [];
 
     for (let i = 0; i < pedidos.length; i++) {
         let obj = pedidos[i];
-        if !(ret.includes(obj.idproducto)) {
+        if (ret.includes(obj.idproducto) == false) {
             let producto = productos.idproducto[obj.idproducto];
             ret.push(
                 {
@@ -21,15 +21,13 @@ function calcularPopularidad(pedidos, productos) {
 
 function pedidosNecesarios( ){
     const pedidos = fetch('https://gist.githubusercontent.com/josejbocanegra/7b6febf87e9d986048a648487b35e693/raw/576531a2d0e601838fc3de997e021816a4b730f8/detallePedido.json')
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then(response => response.json());
 
     const productos = fetch('https://gist.githubusercontent.com/josejbocanegra/be0461060d1c2d899740b8247089ba22/raw/916d2141e32e04031bda79c8886e8e4df0ae7f24/productos.json')
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then(response => response.json());
 
-    calcularPopularidad(pedidos, productos)
-
+    const popularidad = calcularPopularidad(pedidos, productos);
+    console.log(popularidad)
 }
 
 
